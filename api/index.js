5246cc7623eb3deb629767dc7db48e59204719ba
Hanna <hanna.flores@unt.edu>
2021-02-09 21:40:28 -0600

    add mongoose thing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 var express = require("express");
+var mongoose = require("mongoose");
 
 var port = process.env.PORT;
 var app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
