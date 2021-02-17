ed006e12938a0e39016123aa702dcb47ccea11ba
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-02-17 15:38:50 -0600

    Added all of login page except picture and added bootstrap dependency

--- client/src/LoginFolder/Login.jsx
+++ client/src/LoginFolder/Login.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import './login.css';
+import { Form, Input, FormGroup, Label, Button } from 'reactstrap';
 
 export default class LoginPage extends React.Component {
     constructor(props) {
@@ -11,8 +13,34 @@ export default class LoginPage extends React.Component {
 
     render() {
         return (
-            <div>
-                LOGGING IN YO
+            <div className={"outer-container"}>
+                <div className={"login-image-container"}>
+                    image here
+                </div>
+                <div className={"login-form-container"}>
+                    <div className={"inner-container"}>
+                        <p className={"login-title"}>Login to the Greenlight Portal</p>
+                        <p className={"login-subtitle"}>Must be part of the UNT System</p>
+                        <div className={"login-form"}>
+                            <Form>
+                                <FormGroup className={"login-text-field"}>
+                                    <Input type="email" name="email" id="email" placeholder="UNT email" />
+                                </FormGroup>
+                                <FormGroup className={"login-text-field"}>
+                                    <Input type="password" name="password" id="password" placeholder="UNT Password" />
+                                </FormGroup>
+                                <FormGroup check className={"login-remember"}>
+                                    <Label check>
+                                    <Input type="checkbox" />{'  '}
+                                    Remember Me
+                                    </Label>
+                                </FormGroup>
+                                <Button style={{backgroundColor: "#008444"}} className={"login-button"} type="submit">Login</Button>
+                            </Form>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
