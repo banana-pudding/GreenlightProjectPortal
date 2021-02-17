e3c18ced3a12b9753d721eccfc4f1fd6f1545b91
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-02-17 16:25:23 -0600

    Added code to create form to send to API on submit. TODO make api call

--- client/src/LoginFolder/Login.jsx
+++ client/src/LoginFolder/Login.jsx
@@ -6,9 +6,35 @@ export default class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            username: "",
+            password: "",
+            remember: false,
             user: {},
             isLoggedIn: false
         }
+    }
+
+    handleUsername = (event) => {
+        this.setState({username: event.target.value})
+    }
+
+    handlePassword = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    handleRemember = (event) => {
+        this.setState({remember: !this.state.remember})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let user = {
+            username: this.state.username,
+            password: this.state.password,
+            remember: this.state.remember
+        }
+
+        alert(`Submitting form:\n\nUsername: ${user.username}\nPassword: ${user.password}\nRemember: ${user.remember}`);
     }
 
     render() {
@@ -22,16 +48,16 @@ export default class LoginPage extends React.Component {
                         <p className={"login-title"}>Login to the Greenlight Portal</p>
                         <p className={"login-subtitle"}>Must be part of the UNT System</p>
                         <div className={"login-form"}>
-                            <Form>
+                            <Form onSubmit={this.handleSubmit}>
                                 <FormGroup className={"login-text-field"}>
-                                    <Input type="email" name="email" id="email" placeholder="UNT email" />
+                                    <Input type="email" name="email" id="email" placeholder="UNT email" onChange={this.handleUsername}/>
                                 </FormGroup>
                                 <FormGroup className={"login-text-field"}>
-                                    <Input type="password" name="password" id="password" placeholder="UNT Password" />
+                                    <Input type="password" name="password" id="password" placeholder="UNT Password" onChange={this.handlePassword}/>
                                 </FormGroup>
                                 <FormGroup check className={"login-remember"}>
                                     <Label check>
-                                    <Input type="checkbox" />{'  '}
+                                    <Input type="checkbox" onChange={this.handleRemember}/>{'  '}
                                     Remember Me
                                     </Label>
                                 </FormGroup>
