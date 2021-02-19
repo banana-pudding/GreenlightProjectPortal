42b1575b6913d5cefc86e0ebe17043d850030f3a
Anthony <ahanel13@gmail.com>
2021-02-19 12:14:33 -0600

    addec about section content and styling

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -7,10 +7,13 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <div id={"landingpage-container"}>
+                {/* Top image with text */}
                 <div className={"hero-image"} style={{backgroundImage: `url("${process.env.PUBLIC_URL }/landing_page_background.png")`}}>
                     <div id={"hero-banner"}></div>
-                        <h1 id={"hero-text"}>Project Greenlight</h1>
-                    </div>
+                    <h1 id={"hero-text"}>Project Greenlight</h1>
+                </div>
+
+                {/* About section  */}
                 <div id={"about-section"}>
                     <h2>About Project Greenlight</h2>
                     <hr  style={{
@@ -21,16 +24,21 @@ export default class LandingPage extends React.Component {
                         width: "20%"
                     }}/>
                     <p>The UNT Greenlight Projects Portal works to serve as a central hub for a repository of carefully curated project concepts by UNT faculty and students.</p>
-                    <hr  style={{
-                        color: '#2B2B2B',
-                        backgroundColor: '#2B2B2B',
-                        height: 1,
-                        borderColor : '#2B2B2B',
-                        width: "80%",
-                        opacity: 0.15
-                    }}/>
                 </div>
-                <div id={"projects-section"}></div>
+                <hr  style={{
+                    color: '#2B2B2B',
+                    backgroundColor: '#2B2B2B',
+                    height: 1,
+                    borderColor : '#2B2B2B',
+                    width: "90%",
+                    opacity: 0.15
+                }}/> 
+
+                {/* Projects Section */}
+                <div id={"projects-section"}>
+                </div>
+
+                {/* How it works section */}
                 <div id={"howitworks-section"}></div>
             </div>
         )
