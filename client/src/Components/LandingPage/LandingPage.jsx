9e4a4ba3994b8f7a1f79905b9bce451af154bd6e
Anthony <ahanel13@gmail.com>
2021-02-19 11:29:56 -0600

    added lines to the landing page. I need to create the columns for formating now

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -3,50 +3,32 @@ import './LandingPage.css';
 // import { Form, Input, FormGroup, Label, Button } from 'reactstrap';
 
 export default class LandingPage extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         username: "",
-    //         password: "",
-    //         remember: false,
-    //         user: {},
-    //         isLoggedIn: false
-    //     }
-    // }
-
-    // handleUsername = (event) => {
-    //     this.setState({username: event.target.value})
-    // }
-
-    // handlePassword = (event) => {
-    //     this.setState({password: event.target.value})
-    // }
-
-    // handleRemember = (event) => {
-    //     this.setState({remember: !this.state.remember})
-    // }
-
-    // handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     let user = {
-    //         username: this.state.username,
-    //         password: this.state.password,
-    //         remember: this.state.remember
-    //     }
-
-    //     alert(`Submitting form:\n\nUsername: ${user.username}\nPassword: ${user.password}\nRemember: ${user.remember}`);
-    // }
 
     render() {
         return (
             <div id={"landingpage-container"}>
                 <div className={"hero-image"} style={{backgroundImage: `url("${process.env.PUBLIC_URL }/landing_page_background.png")`}}>
                     <div id={"hero-banner"}></div>
-                    <h1 id={"hero-text"}>Project Greenlight</h1>
-                </div>
+                        <h1 id={"hero-text"}>Project Greenlight</h1>
+                    </div>
                 <div id={"about-section"}>
                     <h2>About Project Greenlight</h2>
+                    <hr  style={{
+                        color: '#008444',
+                        backgroundColor: '#008444',
+                        height: .5,
+                        borderColor : '#008444',
+                        width: "20%"
+                    }}/>
                     <p>The UNT Greenlight Projects Portal works to serve as a central hub for a repository of carefully curated project concepts by UNT faculty and students.</p>
+                    <hr  style={{
+                        color: '#2B2B2B',
+                        backgroundColor: '#2B2B2B',
+                        height: 1,
+                        borderColor : '#2B2B2B',
+                        width: "80%",
+                        opacity: 0.15
+                    }}/>
                 </div>
                 <div id={"projects-section"}></div>
                 <div id={"howitworks-section"}></div>
