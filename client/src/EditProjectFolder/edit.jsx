59b6f06fa8fd5a3a7dcc277039f649863b5d7baa
Noelle <mansilla.noelle@gmail.com>
2021-02-19 15:14:47 -0600

    added image upload button, adjusted padding/reactive sizing

--- client/src/EditProjectFolder/edit.jsx
+++ client/src/EditProjectFolder/edit.jsx
@@ -1,38 +1,52 @@
 import React from 'react';
 import "./edit.css" 
-import {Container, Row, Col, Form, FormGroup, Label, Input} from 'reactstrap';
+import {Container, Row, Col, Form, FormGroup, FormText, Label, Input} from 'reactstrap';
 
 export default class EditPage extends React.Component {
+	askForSponsor = () => {
+		<Form>
+			<FormGroup>
+				<Label for = "sponsorName" className = 'float-left ml-1 font-weight-bold'>Who is your sponsor?</Label>
+			</FormGroup>
+		</Form>
+	}
+
 	render() {
 			const containerClasses = `themed-container shadow bg-white border border-med`;
-			const labelClasses = 'float-md-left ml-1 font-weight-bold';
+			const labelClasses = 'float-left ml-1 font-weight-bold';
 			const legendClasses = 'col-form-label col-12 font-weight-bold';
 
 			return (
 				<div id = "big-container">
+					{/*Container with form*/}
 					<Container className = {containerClasses} fluid = {true} id = "round-edges">
-						<Row xs = "2">
+						{/*Green title bar*/}
+						<Row xs = "1" sm = "2">
 							<Col id = "rounded-green-bg">
 								<h3>Fancy Form</h3>
 							</Col>
 							<Col></Col>
 						</Row>
-						<Row xs = "2">
+						{/*Form*/}
+						<Row xs = "1" sm = "2">
 							<Col>
 								<Form className = "pt-3">
+									{/*Project title input*/}
 									<FormGroup>
 										<Label for = "projectTitle" className = {labelClasses}>Title</Label>
 										<Input type = "title" name = "title" id = "projectTitle" placeholder = "My Project"/>
 									</FormGroup>
+									{/*Project description input*/}
 									<FormGroup>
 										<Label for = "projectDescription" className = {labelClasses}>Description</Label>
 										<Input type = "textarea" name = "description" id = "projectDescription" placeholder = "Tell us what your project is all about."/>
 									</FormGroup>
+									{/*Sponsor input (Yes/No)*/}
 									<Row xs = "12" className = "ml-1">
 									<FormGroup tag = "fieldset" row className = "float-md-left">
 										<legend className = {legendClasses}>Are you the sponsor of this project?</legend>
 										<Col sm = {10}>
-											<div className = "float-md-left">
+											<div className = "float-left">
 												<FormGroup check>
 													<Label check>
 														<Input type = "radio" name = "radio1"/>{' '}
@@ -40,7 +54,7 @@ export default class EditPage extends React.Component {
 													</Label>
 												</FormGroup>	
 												<FormGroup check>
-													<Label check>
+													<Label check onClick = {this.askForSponsor}>
 														<Input type = "radio" name = "radio1"/>{' '}
 														No
 													</Label>
@@ -49,11 +63,12 @@ export default class EditPage extends React.Component {
 										</Col>
 									</FormGroup>
 									</Row>
+									{/*Recruiting input (Yes/No)*/}
 									<Row xs = "12" className = "ml-1">
-									<FormGroup tag = "fieldset" row className = "float-md-left">
+									<FormGroup tag = "fieldset" row className = "float-left">
 										<legend className = {legendClasses}>Are you recruiting team members?</legend>
 										<Col sm = {10}>
-											<div className = "float-md-left">
+											<div className = "float-left">
 												<FormGroup check>
 													<Label check>
 														<Input type = "radio" name = "radio2"/>{' '}
@@ -70,9 +85,18 @@ export default class EditPage extends React.Component {
 										</Col>
 									</FormGroup>
 									</Row>
+									<FormGroup>
+										<Label for = "projectImage" className = {labelClasses}>Upload an image:</Label>
+										<Input type = "file" name = "file" id = "projectImage"/>
+										<FormText color = "muted" className = "float-left pb-3">
+											This image will be displayed with your project. <br/>
+											Accepted file formats: .jpg, .png, .jpeg
+										</FormText>
+									</FormGroup>
 								</Form>
 							</Col>
-							<Col>
+							{/*Column with content*/}
+							<Col className = "pt-3">
 								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
 							</Col>
 						</Row>
