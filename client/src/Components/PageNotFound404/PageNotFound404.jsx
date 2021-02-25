5c9550dd2398abaa96110fd022aafd13eda8083d
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-02-25 17:20:07 -0600

    Added fancier styling for the 404 page

--- client/src/Components/PageNotFound404/PageNotFound404.jsx
+++ client/src/Components/PageNotFound404/PageNotFound404.jsx
@@ -3,11 +3,21 @@ import { Link } from "react-router-dom";
 import "./PageNotFound404.css";
 
 export default class PageNotFound404 extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div className={"pagenotfound-outer-container"}>
-                <h1>Page not found</h1>
-                <Link to="/">Return Home</Link>
+                <div className="wrapper">
+                    <h1>Hmm.</h1>
+                    <p>It seems that you're lost in a perpetual black hole. Let us help guide you out and get you back home.</p>
+                    <div className="buttons"><a href="#" onClick={()=> this.props.history.goBack()}>back</a><a href="/">home</a><br /><span>Help me out</span></div>
+                </div>
+                <div className="space">
+                    <div className="blackhole"></div>
+                    <div className="ship"></div>
+                </div>
             </div>
         )
     }
